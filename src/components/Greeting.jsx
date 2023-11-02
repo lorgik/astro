f6f143f80a688f99fbc2be22e89790ea1b87c229
lorgik <sib.lorgik@gmail.com>
2023-11-02 16:24:02 +0300

    finished

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function Greeting({ messages }) {
+  const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <div>
+      <h3>{greeting}! Спасибо за ваш визит!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>Новое приветствие</button>
+    </div>
+  );
+}
